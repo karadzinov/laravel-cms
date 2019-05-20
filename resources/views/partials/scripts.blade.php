fab7862418e4cf5e3021e86cc6ce535b0981fdd3
vagrant <vagrant@homestead>
2019-05-20 10:49:42 +0000

    LCMS-4 uses laravel-mix, separates head and scripts in patials

---
+++ resources/views/partials/scripts.blade.php
@@ -0,0 +1,7 @@
+{{-- Scripts --}}
+<script src="{{ mix('assets/js/app.js') }}"></script>
+<script src="{{mix('js/mix.js')}}"></script>
+
+@if(config('settings.googleMapsAPIStatus'))
+    {!! HTML::script('//maps.googleapis.com/maps/api/js?key='.config("settings.googleMapsAPIKey").'&libraries=places&dummy=.js', array('type' => 'text/javascript')) !!}
+@endif
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,26 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/assets/js')
    .sass('resources/assets/sass/app.scss', 'public/assets/css')
+   .styles([
+   		'public/assets/css/bootstrap.min.css',
+   		'public/assets/css/font-awesome.min.css',
+   		'public/assets/css/weather-icons.min.css',
+   		'public/assets/css/beyond.min.css',
+   		'public/assets/css/demo.min.css',
+   		'public/assets/css/skins/orange.min.css',
+   		'public/assets/css/typicons.min.css',
+   		'public/assets/css/animate.min.css',
+   		'public/assets/css/custom.css',
+   		'public/assets/css/skins/orange.css',
+   		'public/assets/css/dataTables.bootstrap.css'
+   		], 'public/css/mix.css')
+   .babel([
+	   	'public/assets/js/jquery.min.js',
+	   	'public/assets/js/bootstrap.min.js',
+	   	'public/assets/js/slimscroll/jquery.slimscroll.min.js',
+	   	'public/assets/js/ckeditor/ckeditor.js',
+	   	'public/assets/js/ckconf.js',
+	   	'public/assets/js/beyond.js',
+	   	'public/assets/js/bootbox/bootbox.js'
+			], 'public/js/mix.js')
    .version();
