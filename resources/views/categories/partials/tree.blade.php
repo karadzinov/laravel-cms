07c4d1b8fb4fd745ac453c8604c41f8e7545c9d3
Nikola <nikola.notevski@sentice.com>
2019-05-21 11:55:06 +0200

    LCMS-6 adds tree for categories

--- resources/views/categories/partials/tree.blade.php
+++ resources/views/categories/partials/tree.blade.php
@@ -1,4 +1,51 @@
 @if ($categories)
+    <ul class="list-group" {{(isset($nestedTable)) ? "style=margin-left:50px;margin-bottom:0" : null}}>
+        @foreach ($categories as $category)
+            @if ( $category->isRoot())
+                <li class="list-group-item">
+                    {{$category->name}} 
+                    <span class="pull-right">
+                        <a href="{{ route('category.show', [ $category->getKey() ]) }}" class="btn btn-info btn-xs edit">
+                            <i class="fa fa-edit"></i>
+                             Show
+                         </a>
+                        <a href="{{ route('category.create', [ 'parent_id' => $category->getKey() ]) }}" class="btn btn-success btn-xs edit">
+                            <i class="fa fa-edit"></i>
+                             Create Child
+                         </a>
+                         <a href="{{ route('category.edit', [ $category->getKey() ]) }}" class="btn btn-warning btn-xs edit">
+                            <i class="fa fa-edit"></i>
+                             Edit
+                         </a>
+                     </span>
+                 </li>
+            @else
+                 <li class="list-group-item">
+                    {{$category->name}} 
+                    <span class="pull-right">
+                        <a href="{{ route('category.show', [ $category->getKey() ]) }}" class="btn btn-info btn-xs edit">
+                            <i class="fa fa-edit"></i>
+                             Show
+                         </a>
+                        <a href="{{ route('category.create', [ 'parent_id' => $category->getKey() ]) }}" class="btn btn-success btn-xs edit">
+                            <i class="fa fa-edit"></i>
+                             Create Child
+                         </a>
+                         <a href="{{ route('category.edit', [ $category->getKey() ]) }}" class="btn btn-warning btn-xs edit">
+                            <i class="fa fa-edit"></i>
+                             Edit
+                         </a>
+                     </span>
+                 </li>
+            @endif
+            @include('categories.partials.tree', [ 'categories' => $category->children, 'nestedTable'=>true ])
+        @endforeach
+    </ul>
+@endif
+
+
+{{-- 
+@if ($categories)
     <ul style="font-size: 13px;">
         @foreach ($categories as $category)
             
@@ -23,4 +70,4 @@
         @endforeach
     </ul>
 @endif
-
+ --}}
