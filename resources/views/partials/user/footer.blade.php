976a8534ac0a07bf5d79d8083b76253cf8864776
vagrant <vagrant@homestead>
2019-06-11 09:50:52 +0000

    LCMS-25 adds basic theme, laravel mix for assets, changes some files to adopt it for the new file structure

---
+++ resources/views/partials/user/footer.blade.php
@@ -0,0 +1,185 @@
+<footer id="footer" class="clearfix ">
+
+	<!-- .footer start -->
+	<!-- ================ -->
+	<div class="footer">
+		<div class="container">
+			<div class="footer-inner">
+				<div class="row">
+					<div class="col-md-3">
+						<div class="footer-content">
+							<div class="logo-footer"><img id="logo-footer" src="{{asset('assets/images/logo_light_blue.png')}}" alt=""></div>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus illo vel dolorum soluta consectetur doloribus sit. Delectus non tenetur odit dicta vitae debitis suscipit doloribus. Ipsa, aut voluptas quaerat... <a href="page-about.html">Learn More<i class="fa fa-long-arrow-right pl-5"></i></a></p>
+							<div class="separator-2"></div>
+							<nav>
+								<ul class="nav nav-pills nav-stacked">
+									<li><a target="_blank" href="http://htmlcoder.me/support">Support</a></li>
+									<li><a href="#">Privacy</a></li>
+									<li><a href="#">Terms</a></li>
+									<li><a href="page-about.html">About</a></li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="footer-content">
+							<h2 class="title">Latest From Blog</h2>
+							<div class="separator-2"></div>
+							<div class="media margin-clear">
+								<div class="media-left">
+									<div class="overlay-container">
+										<img class="media-object" src="{{asset('assets/images/blog-thumb-1.jpg')}}" alt="blog-thumb">
+										<a href="blog-post.html" class="overlay-link small"><i class="fa fa-link"></i></a>
+									</div>
+								</div>
+								<div class="media-body">
+									<h6 class="media-heading"><a href="blog-post.html">Lorem ipsum dolor sit amet...</a></h6>
+									<p class="small margin-clear"><i class="fa fa-calendar pr-10"></i>Mar 23, 2016</p>
+								</div>
+								<hr>
+							</div>
+							<div class="media margin-clear">
+								<div class="media-left">
+									<div class="overlay-container">
+										<img class="media-object" src="{{asset('assets/images/blog-thumb-2.jpg')}}" alt="blog-thumb">
+										<a href="blog-post.html" class="overlay-link small"><i class="fa fa-link"></i></a>
+									</div>
+								</div>
+								<div class="media-body">
+									<h6 class="media-heading"><a href="blog-post.html">Lorem ipsum dolor sit amet...</a></h6>
+									<p class="small margin-clear"><i class="fa fa-calendar pr-10"></i>Mar 22, 2016</p>
+								</div>
+								<hr>
+							</div>
+							<div class="media margin-clear">
+								<div class="media-left">
+									<div class="overlay-container">
+										<img class="media-object" src="{{asset('assets/images/blog-thumb-3.jpg')}}" alt="blog-thumb">
+										<a href="blog-post.html" class="overlay-link small"><i class="fa fa-link"></i></a>
+									</div>
+								</div>
+								<div class="media-body">
+									<h6 class="media-heading"><a href="blog-post.html">Lorem ipsum dolor sit amet...</a></h6>
+									<p class="small margin-clear"><i class="fa fa-calendar pr-10"></i>Mar 21, 2016</p>
+								</div>
+								<hr>
+							</div>
+							<div class="media margin-clear">
+								<div class="media-left">
+									<div class="overlay-container">
+										<img class="media-object" src="{{asset('assets/images/blog-thumb-4.jpg')}}" alt="blog-thumb">
+										<a href="blog-post.html" class="overlay-link small"><i class="fa fa-link"></i></a>
+									</div>
+								</div>
+								<div class="media-body">
+									<h6 class="media-heading"><a href="blog-post.html">Lorem ipsum dolor sit amet...</a></h6>
+									<p class="small margin-clear"><i class="fa fa-calendar pr-10"></i>Mar 21, 2016</p>
+								</div>
+							</div>
+							<div class="text-right space-top">
+								<a href="blog-large-image-right-sidebar.html" class="link-dark"><i class="fa fa-plus-circle pl-5 pr-5"></i>More</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="footer-content">
+							<h2 class="title">Portfolio Gallery</h2>
+							<div class="separator-2"></div>
+							<div class="row grid-space-10">
+								<div class="col-xs-4 col-md-6">
+									<div class="overlay-container mb-10">
+										<img src="{{asset('assets/images/gallery-1.jpg')}}" alt="">
+										<a href="portfolio-item.html" class="overlay-link small">
+											<i class="fa fa-link"></i>
+										</a>
+									</div>
+								</div>
+								<div class="col-xs-4 col-md-6">
+									<div class="overlay-container mb-10">
+										<img src="{{asset('assets/images/gallery-2.jpg')}}" alt="">
+										<a href="portfolio-item.html" class="overlay-link small">
+											<i class="fa fa-link"></i>
+										</a>
+									</div>
+								</div>
+								<div class="col-xs-4 col-md-6">
+									<div class="overlay-container mb-10">
+										<img src="{{asset('assets/images/gallery-3.jpg')}}" alt="">
+										<a href="portfolio-item.html" class="overlay-link small">
+											<i class="fa fa-link"></i>
+										</a>
+									</div>
+								</div>
+								<div class="col-xs-4 col-md-6">
+									<div class="overlay-container mb-10">
+										<img src="{{asset('assets/images/gallery-4.jpg')}}" alt="">
+										<a href="portfolio-item.html" class="overlay-link small">
+											<i class="fa fa-link"></i>
+										</a>
+									</div>
+								</div>
+								<div class="col-xs-4 col-md-6">
+									<div class="overlay-container mb-10">
+										<img src="{{asset('assets/images/gallery-5.jpg')}}" alt="">
+										<a href="portfolio-item.html" class="overlay-link small">
+											<i class="fa fa-link"></i>
+										</a>
+									</div>
+								</div>
+								<div class="col-xs-4 col-md-6">
+									<div class="overlay-container mb-10">
+										<img src="{{asset('assets/images/gallery-6.jpg')}}" alt="">
+										<a href="portfolio-item.html" class="overlay-link small">
+											<i class="fa fa-link"></i>
+										</a>
+									</div>
+								</div>
+							</div>
+							<div class="text-right space-top">
+								<a href="portfolio-grid-2-3-col.html" class="link-dark"><i class="fa fa-plus-circle pl-5 pr-5"></i>More</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3">
+						<div class="footer-content">
+							<h2 class="title">Find Us</h2>
+							<div class="separator-2"></div>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium odio voluptatem necessitatibus illo vel dolorum soluta.</p>
+							<ul class="social-links circle animated-effect-1">
+								<li class="facebook"><a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
+								<li class="twitter"><a target="_blank" href="http://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
+								<li class="googleplus"><a target="_blank" href="http://plus.google.com"><i class="fa fa-google-plus"></i></a></li>
+								<li class="linkedin"><a target="_blank" href="http://www.linkedin.com"><i class="fa fa-linkedin"></i></a></li>
+								<li class="xing"><a target="_blank" href="http://www.xing.com"><i class="fa fa-xing"></i></a></li>
+							</ul>
+							<div class="separator-2"></div>
+							<ul class="list-icons">
+								<li><i class="fa fa-map-marker pr-10 text-default"></i> One infinity loop, 54100</li>
+								<li><i class="fa fa-phone pr-10 text-default"></i> +00 1234567890</li>
+								<li><a href="mailto:info@theproject.com"><i class="fa fa-envelope-o pr-10"></i>info@theproject.com</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- .footer end -->
+
+	<!-- .subfooter start -->
+	<!-- ================ -->
+	<div class="subfooter">
+		<div class="container">
+			<div class="subfooter-inner">
+				<div class="row">
+					<div class="col-md-12">
+						<p class="text-center">Copyright © 2016 The Project by <a target="_blank" href="http://htmlcoder.me">HtmlCoder</a>. All Rights Reserved</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- .subfooter end -->
+
+</footer>
+<!-- footer end -->
