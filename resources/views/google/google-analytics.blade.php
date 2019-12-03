c5e39ddf9dcf19637d349bcf2ae56ffad627a18d
cuzboo <boriscuzovic@gmail.com>
2019-12-03 08:44:53 +0000

    LCMS-63 adds prod google analytics script

--- resources/views/google/google-analytics.blade.php
+++ resources/views/google/google-analytics.blade.php
@@ -1,9 +1,8 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-147159645-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-153791951-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
-  gtag('config', 'UA-147159645-1');
+  gtag('config', 'UA-153791951-1');
 </script>
